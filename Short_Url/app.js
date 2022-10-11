d2e717068b0266e5cf1c75840ea425ff1049443d
jeff0518 <cycivs07@icloud.com>
2022-10-11 15:30:21 +0800

    refactor: modify express routes 重構路由器 Jeff 2022.10.11 15:30

--- Short_Url/app.js
+++ Short_Url/app.js
@@ -1,6 +1,8 @@
 // 基本連線設定
 const express = require("express");
 const app = express();
+// 引用路由器
+const routes = require('./routes')
 const POST = 3000;
 //handlebars
 const exphbs = require("express-handlebars");
@@ -8,16 +10,11 @@ const exphbs = require("express-handlebars");
 app.engine("hbs", exphbs({ defaultLayout: "main", extname: 'hbs'}));
 app.set("view engine", "hbs");
 app.use(express.urlencoded({ extended: true }));
-
+// 將 request 導入路由器
+app.use(routes)
 //設定 Express 路由以提供靜態檔案
 app.use(express.static("public"));
-app.get("/", (req, res) => {
-  res.render("index");
-});
 
-app.get('/copy', (req, res) => {
-  res.render('copy')
-})
 
 app.listen(POST, () => {
   console.log(`App is running on http://localhost:${POST}`);
