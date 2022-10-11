f4894ec47faba268b43fc428012d92567a48edd7
jeff0518 <cycivs07@icloud.com>
2022-10-11 15:11:10 +0800

    feat: add cut version 完成初步畫面設定，新增2個畫面index & copy Jeff 2022.10.11 15:11

--- Short_Url/app.js
+++ Short_Url/app.js
@@ -5,14 +5,20 @@ const POST = 3000;
 //handlebars
 const exphbs = require("express-handlebars");
 
-app.engine("hbs", exphbs({ defaultLayout: "main" }));
+app.engine("hbs", exphbs({ defaultLayout: "main", extname: 'hbs'}));
 app.set("view engine", "hbs");
 app.use(express.urlencoded({ extended: true }));
 
+//設定 Express 路由以提供靜態檔案
+app.use(express.static("public"));
 app.get("/", (req, res) => {
-  res.render("測試用");
+  res.render("index");
 });
 
-app.listen(PORT, () => {
-  console.log(`App is running on http://localhost:${port}`);
+app.get('/copy', (req, res) => {
+  res.render('copy')
+})
+
+app.listen(POST, () => {
+  console.log(`App is running on http://localhost:${POST}`);
 });
